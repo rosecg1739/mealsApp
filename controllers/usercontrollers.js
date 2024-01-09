3d9ab470d904b3d1eda95b8a21fc09c3e432314d
rosecg1739 <chelseagallagher05@gmail.com>
2024-01-09 15:34:27 -0500

    next commit

--- controllers/usercontrollers.js
+++ controllers/usercontrollers.js
@@ -1,6 +1,6 @@
 // Import Dependencies
 const express = require('express')
-const user = require('../models/user')
+const User = require('../models/user')
 const bcrypt = require('bcryptjs')
 const mongoose = require('../utils/connection')
 
@@ -22,6 +22,29 @@ router.get('/signup', (req, res) => {
 
 // Post -> Signup
 
+router.post('/signup', async(req, res) => { 
+    const { username, loggedIn, userId } = req.session
+   
+
+    const newUser =  req.body
+    
+
+
+
+    newUser.password = await bcrypt.hashSync(
+        newUser.password,
+         await bcrypt.genSaltSync(10)
+    )
+   // now can creat user
+
+   User.create(newUser)
+   .then(user => {
+      res.redirect('/users/login')
+   })
+   .catch(error => {
+         console.log('error')
+            res.send('somtheing went wrong')
+   })
 // GET -> Login
 
 router.get('/login', (req, res) => {
@@ -32,6 +55,36 @@ router.get('/login', (req, res) => {
 
 // POST -> Login
 
+router.post('/login', async (req, res) => {
+    const { username, password } = req.body
+
+    User.findOne({ username })
+      .then(async (user) => {
+        if (user) {
+            const result = await bcrypt.compare(password, user.password)
+
+            if (result) {
+                req.session.username = username
+                req.session.loggedIn = true
+                req.session.userId = user.id
+
+                res.redirect('/')
+            } else {
+                res.send('something went wrong')
+            }
+        } else {
+            res.send('wrong password')
+        }
+      }) // Add closing brace here
+
+    try {
+        // code inside the catch statement
+    } catch (error) {
+        console.error(error);
+        res.send('An error occurred: ' + error.message);
+    }
+})
+
 // GET -> Logout - /user/logout
 
 router.get('/logout', (req, res) => {
@@ -40,9 +93,13 @@ router.get('/logout', (req, res) => {
     res.render('users/logout', { username, loggedIn, userId })
 })
 
-
 // Delete -> Logout
-
+router.delete('/logout', (req, res) => {
+    req.session.destroy(() => {
+        res.redirect('/')
+    })
+})
+})
 
 // Export our Router
 
